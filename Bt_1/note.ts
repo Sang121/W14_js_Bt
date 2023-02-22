01195cb5403346c809fe8272bc0052eb075b1fb9
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-02-22 17:05:13 +0700

    bai tap 1

--- Bt_1/note.ts
+++ Bt_1/note.ts
@@ -27,14 +27,13 @@ let a=new Note()
  a.addNote("Các việc cần làm "," Code")
  let noteList1=new NoteLists("TodoList");
 console.log( `Tạo note`);
-console.log(`Title: ${ a.titles}\n Content: ${a.content}`);
-
+console.log(`Title: ${ a.titles}\n Content: ${a.content}`); //Title: Các việc cần làm Content:  Code
 
 console.log( `add note to list:`)
 
 noteList1.addNotetoList(a)
-console.log(`Name NoteList ${ noteList1.name}`);
-console.log( noteList1.Notes[0]);
+console.log(`Name NoteList ${ noteList1.name}`); //Name NoteList TodoList
+console.log( noteList1.Notes[0]); //Note { titles: 'Các việc cần làm ', content: ' Code' }
 
 
 
